b1eb18bdc331ced910dbbe3b7f8a86d98705d107
yuseirf <yusei.tanaka2112325@icloud.com>
2021-07-05 17:04:25 +0900

    必要なデータ取得できました

--- test.py
+++ test.py
@@ -3,76 +3,103 @@ from selenium.webdriver import Chrome
 
 import csv
 import time
+import re
+
+# 完成したら削除
 import pdb
+import json
+
 
 replace_table = str.maketrans({
     '\u3000': '',
-    ' ': ''
+    ' ': '',
+    '\n': ''
 })
 
 item_labels = [
     'company_name',
     'location',
+    'pref',
     'receptionist',
     'type_of_work',
     'employment_status',
     'phone_number',
-    'mail_address'
+    'mail_address',
+    'job_advance_url'
 ]
 
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+
+page_urls = []
 start_url = 'https://jobadvance.jp/category/%e5%85%a8%e5%9b%bd/'
-driver.get(start_url)
+page_urls.append(start_url)
 
-article_urls = []
-jobs = driver.find_elements_by_css_selector('ol#post_list2 li.article')
-for job in jobs:
-    article_url = job.find_element_by_css_selector('a').get_attribute('href')
-    article_urls.append(article_url)
-    time.sleep(3)
+for page_url in page_urls:
+    driver.get(page_url)
+    next_page = driver.find_element_by_css_selector('li.next > a.next.page-numbers').get_attribute('href')
+    if next_page is not None:
+        page_urls.append(next_page)
 
-for article_url_list in article_urls:
-    driver.get(article_url_list)
-    item = {}
-    table_tr_selectors = driver.find_elements_by_css_selector('table.wp-block-table > tbody > tr')
-    for table_tr_selector in table_tr_selectors:
-        content_name = table_tr_selector.find_element_by_css_selector('td:nth-of-type(1)').text
-        content_data = table_tr_selector.find_element_by_css_selector('td:nth-of-type(2)').text.translate(replace_table)
-        if "会社名" in content_name:
-            item['company_name'] = content_data
-            company_name = item['company_name']
-        if "所在地" in content_name:
-            item['location'] = content_data
-            location = item['location']
-        if "受付担当者" in content_name:
-            item['receptionist'] = content_data.replace('採用担当 / ', '')
-            receptionist = item['receptionist']
-        if "職種" in content_name:
-            item['type_of_work'] = content_data
-            type_of_work = item['type_of_work']
-        if "雇用形態" in content_name:
-            item['employment_status'] = content_data
+    article_urls = []
+    jobs = driver.find_elements_by_css_selector('ol#post_list2 li.article')
+    for job in jobs:
+        article_url = job.find_element_by_css_selector('a').get_attribute('href')
+        article_urls.append(article_url)
+        time.sleep(3)
 
-    contact_selectors = driver.find_elements_by_css_selector('div.post_content.clearfix')
-    for contact_selector in contact_selectors:
-        phone_number_text_content = contact_selector.find_element_by_css_selector('p:nth-of-type(2)').text
-        phone_number_a_content = contact_selector.find_element_by_css_selector('p > a').text
-        if "電話で応募する：" in phone_number_text_content:
-            item['phone_number'] = phone_number_a_content
-            phone_number = item['phone_number']
+    for article_url_list in article_urls:
+        driver.get(article_url_list)
 
-        mail_address_text_content = contact_selector.find_element_by_css_selector('p:nth-of-type(3)').text
-        mail_address_a_content = contact_selector.find_element_by_css_selector('p:nth-of-type(3) > a').text
-        if mail_address_text_content is not None and "メールで応募する：" in mail_address_text_content:
-            item['mail_address'] = mail_address_a_content
-            mail_address = item['mail_address']
-    print(item)
-    # 取得したitemをcsvファイルに書き込み
-    with open('job_article.csv', 'w', encoding='utf-8') as f:
-        writer = csv.DictWriter(f, fieldnames=item_labels)
-        writer.writeheader()
-        writer.writerow(item)
-    driver.back()
+        item = {}
+        job_advance_url = article_url_list
+        item['job_advance_url'] = job_advance_url
+        time.sleep(3)
+        table_tr_selectors = driver.find_elements_by_css_selector('table.wp-block-table > tbody > tr')
+        location = None
+        for table_tr_selector in table_tr_selectors:
+            content_name = table_tr_selector.find_element_by_css_selector('td:nth-of-type(1)').text
+            content_data = table_tr_selector.find_element_by_css_selector('td:nth-of-type(2)').text.translate(replace_table)
+            if "会社名" in content_name:
+                company_name = content_data
+                item['company_name'] = company_name
+            if "所在地" in content_name:
+                location = content_data
+                item['location'] = location
+                pref = re.match('東京都|北海道|(?:京都|大阪)府|.{2,3}県', location).group()
+                item['pref'] = pref
+            if "受付担当者" in content_name:
+                receptionist = content_data.replace('採用担当/', '')
+                item['receptionist'] = receptionist
+            if "職種" in content_name:
+                type_of_work = content_data
+                item['type_of_work'] = type_of_work
+            if "雇用形態" in content_name:
+                employment_status = content_data
+                item['employment_status'] = employment_status
 
+        contact_selectors = driver.find_elements_by_css_selector('div.post_content.clearfix')
+        for contact_selector in contact_selectors:
+            if len(contact_selector.find_elements_by_css_selector('p:nth-of-type(2)')) > 0:
+                phone_number_text_content = contact_selector.find_element_by_css_selector('p:nth-of-type(2)').text
+                phone_number_a_content = contact_selector.find_element_by_css_selector('p > a').text
+                if "電話で応募する：" in phone_number_text_content:
+                    phone_number = phone_number_a_content
+                    item['phone_number'] = phone_number
+            if len(contact_selector.find_elements_by_css_selector('p:nth-of-type(3)')) > 0:
+                mail_address_text_content = contact_selector.find_element_by_css_selector('p:nth-of-type(3)').text
+                mail_address_a_content = contact_selector.find_element_by_css_selector('p:nth-of-type(3) > a').text
+                if "メールで応募する：" in mail_address_text_content:
+                    mail_address = mail_address_a_content
+                    item['mail_address'] = mail_address
+        # 完成したら削除
+        item_elem = json.dumps(item, ensure_ascii=False)
+        print(item_elem.replace(',', ',\n'))
+
+        driver.back()
+
+        with open('job_article.csv', 'w', newline='', encoding='utf-8') as f:
+            writer = csv.DictWriter(f, fieldnames=item_labels)
+            writer.writeheader()
+            writer.writerow(item)
 
 driver.close()
